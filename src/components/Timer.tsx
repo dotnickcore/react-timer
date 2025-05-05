56b4150d3479e4a3de90572cc9aafc5840df6b9a
trekswithnick <nicholasbrowningdev@outlook.com>
2025-05-05 22:11:27 +1000

    added progress bar, will continue after work tomorrow

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,19 @@
 import Container from './UI/Container.tsx';
 import { type Timer as TimerProps } from '../types/Timer.ts'
+import { useState } from 'react';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration);
+  
+  setInterval(function() {
+    setRemainingTime(prevTime => prevTime - 50);
+  }, 50);
+  
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p><progress max={duration * 1000} value={remainingTime}/></p>
+      <p>{}</p>
     </Container>
   );
 }
